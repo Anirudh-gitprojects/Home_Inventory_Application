f353586d02c6e1ada9fe6c8279f529ddd568bbc5
AnirudhsDOCS <30415392+Anirudh-gitprojects@users.noreply.github.com>
2023-12-07 19:39:32 +0530

    Update GoogleAuth.js

--- src/pages/GoogleAuth.js
+++ src/pages/GoogleAuth.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, Alert,Dimensions} from "react-native";
-import { AntDesign } from '@expo/vector-icons';
 import {
-  GoogleSignin,
-} from "@react-native-google-signin/google-signin";
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  Dimensions,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import Home from "./Inventory_Display";
-var {height} = Dimensions.get('window');
+var { height } = Dimensions.get("window");
 
 // npx expo install @react-native-google-signin/google-signin
 // npx expo install expo-dev-client
@@ -15,20 +20,19 @@ var {height} = Dimensions.get('window');
 export default function GoogleAuth() {
   const [error, setError] = useState();
   const [userInfo, setUserInfo] = useState();
- const navigation=useNavigation();
+  const navigation = useNavigation();
   const configureGoogleSignIn = () => {
     GoogleSignin.configure({
-
       androidClientId:
         "947458477697-vhi5ndj2qslvgop5q4hu4s1in2q4okih.apps.googleusercontent.com",
-      iosClientId:"[ios-id here]"
+      iosClientId: "[ios-id here]",
     });
   };
 
   useEffect(() => {
     configureGoogleSignIn();
   });
-  
+
   const signIn = async () => {
     console.log("Pressed sign in");
 
@@ -38,7 +42,7 @@ export default function GoogleAuth() {
       setUserInfo(userInfo);
       setError();
     } catch (e) {
-      Alert.alert('Erorr Message','Sign in canceled.')
+      Alert.alert("Erorr Message", "Sign in canceled.");
     }
   };
 
@@ -50,17 +54,17 @@ export default function GoogleAuth() {
 
   return (
     <View style={styles.container}>
-
       {userInfo ? (
         <>
-        <Home logout={logout} userInfo={userInfo}/>
+          <Home logout={logout} userInfo={userInfo} />
         </>
       ) : (
-        <TouchableOpacity
-          style={styles.googleBtn}
-          onPress={signIn}>
-        
-        <View style={styles.btnDisplay}><Text style={styles.text}>Sign in with</Text><AntDesign name="google" size={24} color="black" /></View></TouchableOpacity>
+        <TouchableOpacity style={styles.googleBtn} onPress={signIn}>
+          <View style={styles.btnDisplay}>
+            <Text style={styles.text}>Sign in with</Text>
+            <AntDesign name="google" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
       )}
 
       <StatusBar style="auto" />
@@ -70,35 +74,31 @@ export default function GoogleAuth() {
 
 const styles = StyleSheet.create({
   container: {
-  
-    alignItems: "center", 
+    alignItems: "center",
     justifyContent: "center",
-    flex:height
+    flex: height,
   },
-  btnDisplay:{
-    flexDirection:'row',
-    justifyContent:'space-evenly'
+  btnDisplay: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
-  logout:{
-    marginBottom:0,
-    marginTop:65,
-    alignSelf:'flex-end'
+  logout: {
+    marginBottom: 0,
+    marginTop: 65,
+    alignSelf: "flex-end",
   },
-  text:{
-   
-    color:'black',
-    marginLeft:50,
-    fontSize:16,
-    alignSelf:'center',
-  
+  text: {
+    color: "black",
+    marginLeft: 50,
+    fontSize: 16,
+    alignSelf: "center",
   },
-  googleBtn:{
-    borderWidth:1,
-    justifyContent:'center',
-    width:300,
-    borderRadius:20,
-    height:50,
-    backgroundColor:'ghostwhite',
-
-  }
+  googleBtn: {
+    borderWidth: 1,
+    justifyContent: "center",
+    width: 300,
+    borderRadius: 20,
+    height: 50,
+    backgroundColor: "ghostwhite",
+  },
 });
